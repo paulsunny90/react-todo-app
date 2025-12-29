15a13c363f51b65e64569f5dda5d9eef9917ccff
paulsunny90 <paulsunny87267@gmail.com>
2025-12-29 11:54:31 +0530

    crated upadtetodod

--- src/context/todocontext.jsx
+++ src/context/todocontext.jsx
@@ -20,8 +20,24 @@ export const TodoProvider = ({ children }) => {
   const deleteTodo = (id) => {
     setTodos(todos.filter(todo => todo.id !== id));
   };
-
   
+  const updateTodo = (id, newText) => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, text: newText } : todo
+      )
+    );
+  };
+
+  return (
+    <TodoContext.Provider value={{ todos, addTodo, deleteTodo, updateTodo }}>
+      {children}
+    </TodoContext.Provider>
+  );
+
 };
 
+
+
 export const useTodos = () => useContext(TodoContext);
+
